c237ec9fe6d75aa33a7c7697494e65659b5f0dff
Jorge Garcia <tgarcia5920@gmail.com>
2024-12-26 20:01:49 -0500

    picture taker
    data_collector works now

--- src/img_collect.py
+++ src/img_collect.py
@@ -7,10 +7,10 @@ if not os.path.exists(data):
     os.makedirs(data)
 
 size_dataset = 100
-amountClass = 3
+amountClass = 26    
 
 
-capture = cv2.VideoCapture(2)
+capture = cv2.VideoCapture(0)
 
 for i in range(amountClass):
     if not os.path.exists(os.path.join(data, str(i))):
@@ -18,6 +18,25 @@ for i in range(amountClass):
 
     print(f"Collecting class {i}")
 
+    done = False
+    while True:
+        ret, frame = capture.read()
+        cv2.putText(frame, "press q to start", (100, 50), cv2.FONT_HERSHEY_PLAIN, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(25) == ord("q"):
+            break
     
+    counter = 0 
+
+    while counter < size_dataset:
+        ret, frame = capture.read()
+        cv2.imshow("frame", frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(data, str(i), f"{counter}.jpg"), frame)
+
+        counter += 1
+
+capture.release()
+cv2.destroyAllWindows()
 
  
