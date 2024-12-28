38e80360e5733a8a8f76002a0b916886d100eec0
Jorge Garcia <tgarcia5920@gmail.com>
2024-12-28 16:43:15 -0500

    live hands baby
    oh yeah

--- src/eyes.py
+++ src/eyes.py
@@ -2,22 +2,30 @@ import cv2
 import mediapipe as mp 
 
 cam = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 while True:
     ret, frame = cam.read()
     
-    mp_hands = mp.solutions.hands
-    mp_draw = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
+    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-
+    results = hands.process(img_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style()    
+            )
     
     cv2.imshow("frame", frame)
     cv2.waitKey(25)
-
-
-
 
 cam.release()
 cv2.destroyAllWindows()
