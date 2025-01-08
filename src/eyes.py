242cd214436189c26cfeaa6cfdcde552f2ef879a
Jorge Garcia <tgarcia5920@gmail.com>
2025-01-08 13:09:17 -0500

    Update eyes.py

--- src/eyes.py
+++ src/eyes.py
@@ -18,12 +18,6 @@ labls_dict = {
 0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J", 10: "K", 11: "L", 12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R", 18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X", 24: "Y", 25: "Z",
 }
 
-
-# def custom1():
-#     return mp_drawing_styles.DrawingSpec(color=(0, 255, 0), thickness=5, circle_radius=5)
-# def custom2():
-#     return mp_drawing_styles.DrawingSpec(color=(255, 0, 0), thickness=3)
-
 while True:
     data_aux = []
     ret, frame = cam.read()
